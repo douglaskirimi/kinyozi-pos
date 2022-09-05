793795640d08d88169fbd9f10f86ea44e3283546
~Douglas <douglaskirimi98@gmail.com>
2022-09-06 02:32:10 +0300

    updates

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -14,7 +14,7 @@
                 <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="media align-items-center">
                         <span class="avatar avatar-sm rounded-circle">
-                           <img alt="Image placeholder" src="{{ asset('argon') }}/img/douglas.JPG">
+                           <img alt="Image placeholder" src="{{ asset('argon') }}/img/douglas.jpg">
                         </span>
                     </div>
                 </a>
