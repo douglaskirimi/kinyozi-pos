acc6014000b6a5dbba64144f35b7f35ca5c15aea
~Douglas <douglaskirimi98@gmail.com>
2022-09-05 05:03:51 +0300

    Recent Changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,6 +67,9 @@
 <!-- <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> -->
 <!-- <script src="https://unpkg.com/axios/dist/axios.min.js"></script> -->
 <script src="{{ asset('assets/js/app.js') }}"></script>
+
+<!-- Axios CND -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js" integrity="sha512-u9akINsQsAkG9xjc1cnGF4zw5TFDwkxuc9vUp5dltDWYCSmyd0meygbvgXrlc/z7/o4a19Fb5V0OUE58J7dcyw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script type="text/javascript">
     document.getElementById('getAccessToken').addEventListener('click',(event) =>{
         event.preventDefault();
