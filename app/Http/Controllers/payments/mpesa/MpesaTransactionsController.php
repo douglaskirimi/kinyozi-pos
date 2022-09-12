07fc21110eb577bbf6eb02efff33f9ee0afd68f0
~Douglas <douglaskirimi98@gmail.com>
2022-09-12 10:48:14 +0300

    callback url

--- app/Http/Controllers/payments/mpesa/MpesaTransactionsController.php
+++ app/Http/Controllers/payments/mpesa/MpesaTransactionsController.php
@@ -76,7 +76,7 @@ class MpesaTransactionsController extends Controller
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-        if($curl_response = curl_exec($curl)) {  
+        $curl_response = curl_exec($curl);
             dd($curl_response);
             $stkPullResponse = json_decode($curl_response);
             $stkResCode  = $stkPullResponse->ResponseCode;
@@ -87,10 +87,6 @@ class MpesaTransactionsController extends Controller
         else{
             return null;
         }
-     }
-     else{
-     	return "Error 109";
-     }
  
     }
 
